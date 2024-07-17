37e3dcfb33804eab6170f97addb76b1d1c6c91a2
Daniel Chin <DanChin327@gmail.com>
2024-07-17 14:24:19 +0900

    working on js loops

--- exercises/loops_exercise.js
+++ exercises/loops_exercise.js
@@ -4,7 +4,7 @@
 
 // For this I will provide basic algorithmic thinking and design process so you understand what you should do before starting any actual coding project.
 
-// 1. Setup Initial Variable (need at least 2 Numbers and one Boolean)
+// 1. Setup Initial Variable (need at least 2 Numbers and one Boolean) DONE
 // 2. Generate the random target number.
 // 3. Start the game loop
 // 4. Prompt for the guess
@@ -18,6 +18,26 @@
 // - Having a play again option
 // - Setting up extra validations for user input (within the correct range)
 // - Creating a different set of difficulties and a prompt for difficulty level
+
+let guessGame = () => {
+let randNum = Math.floor(((Math.random() * 100) + 1))
+  while(true){
+    let guess = parseInt(prompt("Guess a number from 1 to 100: "))
+    if (isNaN(guess)) {
+      console.log(`Invalid input. You need to use a number.`)
+    }
+    else if (randNum === guess) {
+      console.log(`Correct! the number was ${randNum}!`)
+      break;
+    }
+    else if (randNum > guess) {
+      console.log(`Incorrect. Your guess was too low.`)
+    }
+    else if (randNum < guess) {
+      console.log(`Incorrect. Your guess was too high.`)
+    }
+  }
+}
 
 
 
@@ -38,7 +58,49 @@
 
 
 
+let game = () => {
+let numOfGames = prompt('Best out of how many games? ')
+numOfGames = parseInt(numOfGames)
+let choices = ['rock', 'paper', 'scissors']
+let computerCounter = 0;
+let playerCounter = 0;
 
+while (computerCounter < numOfGames && playerCounter < numOfGames) {
+let randNum = Math.floor((Math.random() * 3))
+let compChoice = choices[randNum]
+let playerChoice = prompt("rock, paper, or scissors")
+  if (playerChoice !== 'rock' && playerChoice !=='paper' && playerChoice !== 'scissors') {
+    console.log(`not accepted input. you put in ${playerChoice}.\n`)
+  }
+  else if (compChoice === playerChoice) {
+    console.log(`Tie. Both chose ${playerChoice}.`)
+  }
+  else if ((compChoice === 'rock' && playerChoice === 'scissors')||(compChoice === 'paper' && playerChoice === 'rock') || (compChoice === 'scissors' && playerChoice === 'paper')) {
+    console.log(`Computer Wins. Computer chose ${compChoice}. Player chose ${playerChoice}.\n`)
+    computerCounter += 1;
+  }
+  else if ((compChoice === 'scissors' && playerChoice === 'rock')||(compChoice === 'rock' && playerChoice === 'paper') || (compChoice === 'paper' && playerChoice === 'scissors')) {
+    console.log(`Player Wins. Computer chose ${compChoice}. Player chose ${playerChoice}.\n`)
+    playerCounter += 1;
+  }
+  else{
+    console.log(`Computer Choice: ${compChoice} / Player Choice: ${playerChoice}`)
+  }
+}
+console.log(`Scores-> Computer:${computerCounter} and Player:${playerCounter}.\n`)
+
+while(true){
+  let playGame = prompt("Play Game? (yes/no)")
+  if (playGame === 'no'){
+    break;
+  }
+  else {
+    game();
+  }
+}
+}
+
+// game();
 //   ## Drawing a Pyramid  - Nested For Loop practice
 
 // This is a VERY common challenge in coding to test the understanding of loops/logic and how it works.
@@ -52,3 +114,10 @@
 // This requires MULTIPLE nested loops.
 
 // - If you want a real challenge, have it output a diamond instead.
+
+
+// let result = ''
+// for (let i = 0; i < 5; i ++) {
+//   result += "x"
+//   console.log(result)
+// }
