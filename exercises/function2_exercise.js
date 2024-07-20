dfb0b81298eba1b9a0749532fd94207d35fac934
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2024-07-20 19:20:02 +0900

    working on functions

--- exercises/function2_exercise.js
+++ exercises/function2_exercise.js
@@ -9,33 +9,33 @@
 
 
 let chooseDifficulty = (difficulty) =>{
-  while(true){
-    difficulty= prompt("choose a difficulty: (easy/mid/hard)")
-    if (difficulty !== "easy" && difficulty !== "mid" && difficulty !== "hard") {
-      console.log (`Error input. Wrote ${difficulty}. Write easy / mid / hard.`)
-    }
-    else {
-      return difficulty;
-    }
-  }
+while(true){
+difficulty= prompt("choose a difficulty: (easy/mid/hard)")
+if (difficulty !== "easy" && difficulty !== "mid" && difficulty !== "hard") {
+    console.log (`Error input. Wrote ${difficulty}. Write easy / mid / hard.`)
 }
-
+else {
+    return difficulty;
+}
+}
+}
+    
 let randomNum = () => {
-  let difficulty = chooseDifficulty();
-  let randomNum;
-  switch (difficulty) {
-    case "easy":
-    randomNum = (Math.floor(Math.random() * 5) + 1)
-    break
-    case "mid":
-    randomNum = (Math.floor(Math.random() * 20) + 1)
-    breaks
-    case "hard":
-    randomNum = (Math.floor(Math.random() * 100) + 1)
-    break
-    default:
-    console.log("bug in randomNum Function")
-    break;
-  }
-  return randomNum;
+let difficulty = chooseDifficulty();
+let randomNum;
+switch (difficulty) {
+case "easy":
+randomNum = (Math.floor(Math.random() * 5) + 1)
+break
+case "mid":
+randomNum = (Math.floor(Math.random() * 20) + 1)
+breaks
+case "hard":
+randomNum = (Math.floor(Math.random() * 100) + 1)
+break
+default:
+console.log("bug in randomNum Function")
+break;
+}
+return randomNum;
 }
