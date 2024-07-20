b18a8689470ee6be89e54caac0ec7771512072e1
Daniel Chin <DanChin327@gmail.com>
2024-07-20 15:40:39 +0900

    added random Num

--- exercises/function2_exercise.js
+++ exercises/function2_exercise.js
@@ -1,1 +1,41 @@
-// Write a Rock Paper Scissor Game Using Functions:
+// Write out three different things using functions
+
+
+// Write out a guess the number game
+// Write out a rock paper scissor game
+
+
+// Guess out the number
+
+
+let chooseDifficulty = (difficulty) =>{
+  while(true){
+    difficulty= prompt("choose a difficulty: (easy/mid/hard)")
+    if (difficulty !== "easy" && difficulty !== "mid" && difficulty !== "hard") {
+      console.log (`Error input. Wrote ${difficulty}. Write easy / mid / hard.`)
+    }
+    else {
+      return difficulty;
+    }
+  }
+}
+
+let randomNum = () => {
+  let difficulty = chooseDifficulty();
+  let randomNum;
+  switch (difficulty) {
+    case "easy":
+    randomNum = (Math.floor(Math.random() * 5) + 1)
+    break
+    case "mid":
+    randomNum = (Math.floor(Math.random() * 20) + 1)
+    breaks
+    case "hard":
+    randomNum = (Math.floor(Math.random() * 100) + 1)
+    break
+    default:
+    console.log("bug in randomNum Function")
+    break;
+  }
+  return randomNum;
+}
