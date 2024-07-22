c79eba71acfcae5ef17ea3788a701090f63142a3
Daniel Chin <DanChin327@gmail.com>
2024-07-22 14:05:03 +0900

    starting on arrays on objects

---
+++ exercises/arrays_and_objects_exercise.js
@@ -0,0 +1,41 @@
+// # Exercises on Arrays
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// You will have to look up and read some of the methods that you can use on arrays.
+// ### **Array Initialization and Basic Operations**
+
+// - **Objective**: Create an array of numbers and perform basic operations.
+// - **Instructions**:
+//     1. Create an array of 5 numbers.
+//     2. Add a new number to the end of the array.
+//     3. Remove the first number from the array.
+//     4. Calculate and print the sum of all numbers in the array.
+
+// ### **Array Filtering and Mapping**
+
+// - **Objective**: Use array methods `filter` and `map`.
+// - **Instructions**:
+//     1. Create an array of 10 random integers.
+//     2. Use `filter` to create a new array containing only even numbers.
+//         1. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+//     3. Use `map` to create a new array where each number is squared.
+//         1. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+// ### **Array Sorting and Reversing**
+
+// - **Objective**: Sort and reverse an array.
+// - **Instructions**:
+//     1. Create an array of strings.
+//     2. Sort the array alphabetically.
+//     3. Reverse the sorted array.
+
+// ### **2D Array Manipulation**
+
+// - **Objective**: Work with a 2D array.
+// - **Instructions**:
+//     1. Create a 2D array (an array of arrays) representing a grid of numbers.
+//     2. Print each row of the grid.
+//     3. Calculate the sum of all elements in the grid.
+
+// ![Untitled](https://prod-files-secure.s3.us-west-2.amazonaws.com/97a49e10-c263-4b0e-8110-2e9e07526e7f/a770f05d-c8ab-4339-b1cd-241a84a3bdae/Untitled.png)
