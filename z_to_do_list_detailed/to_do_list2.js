ef3ba6e890a7e7ef1d1807916238d7adb3c0ce53
Daniel Chin <DanChin327@gmail.com>
2024-07-28 15:54:30 +0900

    finished JS on to do list

--- z_to_do_list_detailed/to_do_list2.js
+++ z_to_do_list_detailed/to_do_list2.js
@@ -1,16 +1,16 @@
-const addButton = document.querySelector(".add-new")
+const addButton = document.querySelector('.add-new')
 const theList = document.querySelector('#theList')
 
-
-addButton.addEventListener('click', function() {
-  let message = prompt("Write a new task: ")
-  const newItem = document.createElement('li')
-  newItem.innerHTML = message
+addButton.addEventListener('click', function(){
+  let message = prompt("Write a task: ")
+  let newItem = document.createElement('li')
+  newItem.innerText = message
   theList.appendChild(newItem)
 
-  const deleteButton = document.createElement('button')
-  deleteButton.type="button"
-  deleteButton.textContent='delete'
+  let deleteButton = document.createElement('button')
+  deleteButton.type = 'button'
+  deleteButton.innerText = 'delete'
+  deleteButton.id = 'delete-button'
   newItem.appendChild(deleteButton)
 
   deleteButton.addEventListener('click', function() {
